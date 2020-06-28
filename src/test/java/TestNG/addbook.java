22dd16398aaa7df49b56d140ab58818e35c68599
HrishiGit <66197100+HrishiGit@users.noreply.github.com>
2020-06-29 00:48:54 +0530

    Update addbook.java

--- src/test/java/TestNG/addbook.java
+++ src/test/java/TestNG/addbook.java
@@ -15,7 +15,7 @@ public class addbook {
 	public void launch() {
 		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
 		  driver = new ChromeDriver();
-		  driver.get("http://52.66.248.13:32768/addressbook/");
+		  driver.get("http://3.14.7.67:32768/addressbook/");
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 	}
